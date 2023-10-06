72f66ad22ee9a9353940fcba521feab5018c7d7c
Kristi Cornett <kristi@cornettclan.com>
2023-10-06 15:37:54 -0500

    while loop learning isNullOrEmpty and Trim functions

--- Program.cs
+++ Program.cs
@@ -5,8 +5,11 @@ Your one-stop shop for used sporting equipment.";
 Console.WriteLine(greeting);
 Console.WriteLine("Please enter a product name: ");
 
-string response = Console.ReadLine();
+string response = Console.ReadLine().Trim();
 
-if (string.IsNullOrEmpty(response)){
-    
+while (string.IsNullOrEmpty(response))
+{
+    Console.WriteLine("You didn't choose anything, try again!");
+    response = Console.ReadLine().Trim();
 }
+Console.WriteLine($"You chose: {response}");
